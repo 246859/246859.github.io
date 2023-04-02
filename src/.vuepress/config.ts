141b59513eb9b73132b3e50bdf21be539188234c
246859 <2633565580@qq.com>
2023-04-02 14:44:02 +0800

    feat(config): 新增百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,6 +11,17 @@ export default defineUserConfig({
         [
             "link",
             {rel: "icon", href: "/favicon.ico"},
+        ],
+        [
+            `script`, {}, `
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?87276829dc72e9eeef7c53e10550b458";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+`
         ]
     ],
     locales: {
